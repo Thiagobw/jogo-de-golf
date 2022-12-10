37988d1bfd3858415acf180c11516ff2f128a0f4
Thiagobw <96150000+Thiagobw@users.noreply.github.com>
2022-12-10 13:26:37 -0300

    Revert "adição de scripts"
    This reverts commit ae537f97e6829260212c6226324e326e339a1f3b.

--- Assets/scripts/moveBolinha.cs
+++ Assets/scripts/moveBolinha.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Teste : MonoBehaviour
+public class moveBolinha : MonoBehaviour
 {
-public float x, y;
+    public float x, y;
     float velocity;
     LineRenderer lr;
     Rigidbody rb;
